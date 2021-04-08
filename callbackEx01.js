e9d3d752418e3e65e7302f5775ad07405c069f6c
lee <3bnm1128@naver.com>
2021-04-09 01:01:32 +0900

    콜백함수 참고 블로그 좌표

--- callbackEx01.js
+++ callbackEx01.js
@@ -21,3 +21,5 @@ console.log("요청 시작");
 proccess(datas, printResult);
 
 console.log("요청 종료");
+
+// https://joshua1988.github.io/web-development/javascript/javascript-asynchronous-operation/#%EB%B9%84%EB%8F%99%EA%B8%B0-%EC%B2%98%EB%A6%AC-%EA%B7%B8%EA%B2%8C-%EB%AD%94%EA%B0%80%EC%9A%94
